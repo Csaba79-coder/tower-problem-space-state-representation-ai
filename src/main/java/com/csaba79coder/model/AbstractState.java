02bdd3417b5b330cac06093b2be327579c42f0ae
webler-biology-physiology <csabavadasz79@gmail.com>
2023-11-30 16:25:13 +0100

    change the access modifier to protected, as the rules (child is in a different package!)

--- src/main/java/com/csaba79coder/model/AbstractState.java
+++ src/main/java/com/csaba79coder/model/AbstractState.java
@@ -6,11 +6,11 @@ package com.csaba79coder.model;
 public abstract class AbstractState implements Cloneable {
 
     // Checks if the internal state is a state.
-    abstract boolean isState();
+    protected abstract boolean isState();
     // Checks if the internal state is a goal state.
-    abstract boolean isGoalState();
+    protected abstract boolean isGoalState();
     // Returns the number of operators available.
-    abstract int getOperatorCount();
+    protected abstract int getOperatorCount();
     // Access all operators through the super operator.
     // Returns true if the i-th base operator can be applied to the internal state.
     // Call this method from a for loop, starting from 0 to the number of base operators.
@@ -21,7 +21,7 @@ public abstract class AbstractState implements Cloneable {
     //         System.out.println("The " + i + "th operator can be applied to the state " + state);
     //     }
     // }
-    abstract boolean isSuperOperator(int i);
+    protected abstract boolean isSuperOperator(int i);
     // Clone method. This method is required because we may need to undo the effects of some operators.
     // The simplest way is to clone the state, apply the operator to the clone, and check for problems.
     // If there are no issues, the clone becomes the new state for further search.
